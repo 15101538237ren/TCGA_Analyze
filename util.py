32f7752668a32a409402c2ae16178109110746da
renhonglei <renhongleiz@126.com>
2017-11-30 10:30:10 +0800

    1130

--- util.py
+++ util.py
@@ -431,63 +431,88 @@ def query_stage_of_an_submitter_id(submitter_ids, query_size):
             print e
     print submitter_id_to_stage
     return [submitter_id_to_stage]
+
+def input_submitter_id_and_its_stages(input_cancer_dir, cancer_name):
+    submitter_id_to_stage_dict = {}
+    submitter_id_input_filepath = os.path.join(input_cancer_dir, cancer_name + "_submitter_ids.txt")
+    stages_input_filepath = os.path.join(input_cancer_dir, cancer_name + "_stages.txt")
+    submitter_ids = []
+    stages =[]
+    with open(submitter_id_input_filepath,"r") as submitter_id_input_file:
+        line = submitter_id_input_file.readline()
+        while line:
+            submitter_id = line.split("\t")[1]
+            if submitter_id.endswith("\n"):
+                submitter_id = submitter_id[0:-1]
+            submitter_ids.append(submitter_id)
+            line = submitter_id_input_file.readline()
+    with open(stages_input_filepath,"r") as stages_input_file:
+        line = stages_input_file.readline()
+        while line:
+            stage = line.split("\t")[1]
+            if stage.endswith("\n"):
+                stage = stage[0:-1]
+            stages.append(stage)
+            line = stages_input_file.readline()
+    for sidx in range(len(submitter_ids)):
+        submitter_id_to_stage_dict[submitter_ids[sidx]] = stages[sidx]
+    return [submitter_id_to_stage_dict, submitter_ids]
 def dna_mutation_data_transform_pipline():
     outdir ="snv_dat"
     if not os.path.exists(outdir):
         os.makedirs(outdir)
-    #create gene_index_file
-    with open(os.path.join(outdir, "gene_idx.txt"),"w") as gene_idx_file:
-        gene_idx_file.write("\n".join([str(gidx+1) + "\t" + gene for gidx, gene in enumerate(TSG)]))
+    gene_idx_path = os.path.join(outdir, "gene_idx.txt")
+    if not os.path.exists(gene_idx_path):
+        #create gene_index_file
+        with open(gene_idx_path,"w") as gene_idx_file:
+            gene_idx_file.write("\n".join([str(gidx+1) + "\t" + gene for gidx, gene in enumerate(TSG)]))
 
-    colum_idxs = [0, 8, 15, 32] #要提取maf文件的列编号
-    data_pkl_dict = {}
+    colum_idxs = [0, 8, 15] #要提取maf文件的列编号
+    mutation_expections = ["Missense_Mutation", "Translation_Start_Site", "Splice_Region", "Splice_Site", "In_Frame_Del", "In_Frame_Ins", "Frame_Shift_Del", "Frame_Shift_Ins"]
     for cancer_name in cancer_names:
         cancer_dir = os.path.join(snv_base_dir, cancer_name)
+        #只对有突变数据的癌症做分析
         if os.path.exists(cancer_dir):
             data_dict = {stage:{} for stage in merged_stage}
-            uuid_to_stage_dict = {}
-            file_names = os.listdir(cancer_dir)
-            for file_name in file_names:
-                if not file_name.startswith("."):
-                    file_path = os.path.join(cancer_dir,file_name)
-                    snv_file = open(file_path, "r")
-                    print "%s start %s" % (cancer_name, file_path)
-                    # pass head 6 lines
-                    for i in range(6):
-                        snv_file.readline()
-                    line = snv_file.readline()
-                    while line:
-                        line_contents = line.split("\t")
-                        [gene_name, variation_classification, bar_code, uuid] = [line_contents[i] for i in colum_idxs]
-                        submitter_id = "-".join(bar_code.split("-")[0:3])
+            output_cancer_dir = os.path.join(outdir, cancer_name)
+            if os.path.exists(output_cancer_dir):
+                [submitter_id_to_stage_dict, submitter_ids] = input_submitter_id_and_its_stages(output_cancer_dir, cancer_name)
 
-                        if submitter_id not in uuid_to_stage_dict.keys():
-                            uuid_to_stage_dict[submitter_id] = query_stage_of_an_submitter_id(submitter_id)
-                        stage = tumor_stage_convert[uuid_to_stage_dict[submitter_id]]
-                        if gene_name in TSG and variation_classification == "Missense_Mutation":
-                            if submitter_id not in data_dict[stage].keys():
-                                data_dict[stage][submitter_id] = {gene : 0 for gene in TSG}
-                            data_dict[stage][submitter_id][gene_name] += 1
+                for cancer_stage in merged_stage:
+                    for submitter_id in submitter_ids:
+                        data_dict[cancer_stage][submitter_id] = {gene: 0 for gene in TSG}
+
+                file_names = os.listdir(cancer_dir)
+                for file_name in file_names:
+                    if not file_name.startswith("."):
+                        file_path = os.path.join(cancer_dir,file_name)
+                        snv_file = open(file_path, "r")
+                        print "start %s" % file_path
+                        # pass head 6 lines
+                        for i in range(6):
+                            snv_file.readline()
                         line = snv_file.readline()
-                    print "end %s" % file_path
-            data_pkl_dict[cancer_name]=[data_dict, uuid_to_stage_dict]
-    for cancer_name in cancer_names:
-        cancer_dir = os.path.join(snv_base_dir, cancer_name)
-        if os.path.exists(cancer_dir):
-            for cancer_stage in merged_stage_n:
-                submitter_ids = data_dict[cancer_stage].keys()
-                #create sample id file
-                with open(os.path.join(outdir, "Mut_"+cancer_name+"_"+cancer_stage+"_sample_id.txt"),"w") as sample_id_file:
-                    sample_id_file.write("\n".join([str(gidx+1) + "\t" + submitter_id for gidx, submitter_id in enumerate(submitter_ids)]))
-                with open(os.path.join(outdir, "Mut_"+cancer_name+"_"+cancer_stage+".dat"),"w") as data_file:
-                    header = "\t".join([str(item) for item in range(len(submitter_ids) + 1)]) + "\n"
-                    data_file.write(header)
-                    data_str = []
-                    for gidx,gene  in enumerate(TSG):
-                        arr = [gidx + 1]
-                        arr.extend([data_dict[cancer_stage][submitter_id][gene] for submitter_id in submitter_ids])
-                        data_str.append("\t".join([str(item) for item in arr]))
-                    data_file.write("\n".join(data_str))
+                        while line:
+                            line_contents = line.split("\t")
+                            [gene_name, variation_classification, bar_code] = [line_contents[i] for i in colum_idxs]
+                            submitter_id = "-".join(bar_code.split("-")[0:3])
+                            stage = tumor_stage_convert[submitter_id_to_stage_dict[submitter_id]]
+
+                            if gene_name in TSG and (variation_classification in mutation_expections):
+                                data_dict[stage][submitter_id][gene_name] += 1
+                            line = snv_file.readline()
+                        print "end %s" % file_path
+
+                for cancer_stage in merged_stage_n:
+                    with open(os.path.join(output_cancer_dir, cancer_name + "_" + cancer_stage+"_mutation_count.dat"), "w") as data_file:
+                        header = "\t".join([str(item) for item in range(len(submitter_ids) + 1)]) + "\n"
+                        data_file.write(header)
+                        data_str = []
+                        for gidx, gene in enumerate(TSG):
+                            arr = [gidx + 1]
+                            arr.extend([data_dict[cancer_stage][submitter_id][gene] for submitter_id in submitter_ids])
+                            data_str.append("\t".join([str(item) for item in arr]))
+                        data_file.write("\n".join(data_str))
 def get_maf_submitter_ids():
     submitter_dict = {}
     outdir ="snv_dat"
@@ -554,6 +579,6 @@ def get_submitter_id_stages():
                 outfile.write("\n".join([str(gidx+1)  + "\t" + submitter_id_stage_dict[submitter_id] for gidx, submitter_id in enumerate(submitter_ids)])) #+ "\t" + submitter_id
                 print "write %s successful" % input_path
 if __name__ == '__main__':
-    get_maf_submitter_ids()
-    get_submitter_id_stages()
-    # dna_mutation_data_transform_pipline()
+    # get_maf_submitter_ids()
+    # get_submitter_id_stages()
+    dna_mutation_data_transform_pipline()
